9ed32472c845301bf497438dd384f985944953d3
wade ryan <wryan67@gmail.com>
2020-07-26 16:33:49 -0500

    window management

---
+++ remoteBuild.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [ "$*" = "" ];then
+    echo "usage: remoteBuild.sh [commit message]"
+    exit 9
+fi  
+
+git commit -am "$*" && git push && \
+ssh morpheus "cd /home/wryan/projects/udd_rpi_lib; git pull && make install"
+
