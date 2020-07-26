42b319572718acda2e01f6421a4ddf5af015bd54
wade ryan <wryan67@gmail.com>
2020-07-26 00:39:07 -0500

    window management

--- src/controller/Display.cpp
+++ src/controller/Display.cpp
@@ -188,7 +188,7 @@ namespace udd {
     }
 
     void Display::setWindowFullScreen() {
-        setWindow(0, 0, config.width-1 + config.xOffset, config.height-1 + config.yOffset);
+        setWindow(0, 0, config.width-1 + config.xOffset, config.height-1 + config.yOffset, config.screenRotation);
     }
 
 
