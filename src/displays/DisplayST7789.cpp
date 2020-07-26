d0789a2578277105212f2e8fdab55da82f751e03
wade ryan <wryan67@gmail.com>
2020-07-26 16:04:31 -0500

    window management

--- src/displays/DisplayST7789.cpp
+++ src/displays/DisplayST7789.cpp
@@ -127,7 +127,8 @@ namespace udd {
 
         if (rotation == DEGREE_270) {
             swap(int, y1, y2);
-//            adjustPoint(x1, y1, rotation);
+            swap(int, x1, x2);
+            //            adjustPoint(x1, y1, rotation);
 //            adjustPoint(x2, y2, rotation);
         }
 
